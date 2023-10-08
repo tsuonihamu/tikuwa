7a204e87eaa3b0e029bb77568e3aec3647592e6b
tf <86293351+tsuonihamu@users.noreply.github.com>
2023-10-08 16:41:01 +0900

    Create senyou.js

---
+++ senyou.js
@@ -0,0 +1,102 @@
+function tikuwaresult() {
+	rand = Math.floor(Math.random()*33);
+	if (rand == 0) {
+		document.getElementById("RE1").style.display = "block";
+	};
+	if (rand == 1) {
+		document.getElementById("RE2").style.display = "block";
+	};
+	if (rand == 2) {
+		document.getElementById("RE3").style.display = "block";
+	};
+	if (rand == 3) {
+		document.getElementById("RE4").style.display = "block";
+	};
+	if (rand == 4) {
+		document.getElementById("RE5").style.display = "block";
+	};
+	if (rand == 5) {
+		document.getElementById("RE6").style.display = "block";
+	};
+	if (rand == 6) {
+		document.getElementById("RE7").style.display = "block";
+	};
+	if (rand == 7) {
+		document.getElementById("RE8").style.display = "block";
+	};
+	if (rand == 8) {
+		document.getElementById("RE9").style.display = "block";	
+	};
+	if (rand == 9) {
+		document.getElementById("RE10").style.display = "block";	
+	};
+	if (rand == 10) {
+		document.getElementById("RE11").style.display = "block";	
+	};
+	if (rand == 11) {
+		document.getElementById("RE12").style.display = "block";	
+	};
+	if (rand == 12) {
+		document.getElementById("RE13").style.display = "block";	
+	};
+	if (rand == 13) {
+		document.getElementById("RE14").style.display = "block";	
+	};
+	if (rand == 14) {
+		document.getElementById("RE15").style.display = "block";	
+	};
+	if (rand == 15) {
+		document.getElementById("RE16").style.display = "block";	
+	};
+	if (rand == 16) {
+		document.getElementById("RE17").style.display = "block";	
+	};
+	if (rand == 17) {
+		document.getElementById("RE18").style.display = "block";	
+	};
+	if (rand == 18) {
+		document.getElementById("RE19").style.display = "block";	
+	};
+	if (rand == 19) {
+		document.getElementById("RE20").style.display = "block";	
+	};
+	if (rand == 20) {
+		document.getElementById("RE21").style.display = "block";	
+	};
+	if (rand == 21) {
+		document.getElementById("RE22").style.display = "block";	
+	};
+	if (rand == 22) {
+		document.getElementById("RE23").style.display = "block";	
+	};
+	if (rand == 23) {
+		document.getElementById("RE24").style.display = "block";	
+	};
+	if (rand == 24) {
+		document.getElementById("RE25").style.display = "block";	
+	};
+	if (rand == 25) {
+		document.getElementById("RE26").style.display = "block";	
+	};
+	if (rand == 26) {
+		document.getElementById("RE27").style.display = "block";	
+	};
+	if (rand == 27) {
+		document.getElementById("RE28").style.display = "block";	
+	};
+	if (rand == 28) {
+		document.getElementById("RE29").style.display = "block";	
+	};
+	if (rand == 29) {
+		document.getElementById("RE30").style.display = "block";	
+	};
+	if (rand == 30) {
+		document.getElementById("RE31").style.display = "block";	
+	};
+	if (rand == 31) {
+		document.getElementById("RE32").style.display = "block";	
+	};
+	if (rand == 32) {
+		document.getElementById("RE33").style.display = "block";	
+	};
+}
